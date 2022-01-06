8937a015cc33a10616333b32746e653812fc79f3
Georgios Evangelou <george_evangelou@windowslive.com>
2022-01-06 17:19:23 +0200

    Added fail-safe tests, renamed some tests

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,107 +1,139 @@
-import circuitry.elements.API;
-import circuitry.elements.InputBoolean;
-import circuitry.elements.ElementFactory;
-import circuitry.elements.Circuit;
+import circuitry.elements.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  *
  */
 public class Testing {
     private ElementFactory factory = new ElementFactory(API.VERSION);
-    
-     @Test
-     public void testAnd() {
-         final InputBoolean input1 = factory.createInputBoolean(false);
-         final InputBoolean input2 = factory.createInputBoolean(true);
-         Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
-         assertEquals(false, circuit.calculate());
 
-         input1.set(true);
-         input2.set(true);
-         assertEquals(true, circuit.calculate());
+    @Test
+    public void testAndGate() {
+        final InputBoolean input1 = factory.createInputBoolean(false);
+        final InputBoolean input2 = factory.createInputBoolean(true);
+        Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
+        assertEquals(false, circuit.calculate());
 
-         input1.set(false);
-         input2.set(false);
-         assertEquals(false, circuit.calculate());
+        input1.set(true);
+        input2.set(true);
+        assertEquals(true, circuit.calculate());
 
-         input1.set(true);
-         input2.set(false);
-         assertEquals(false, circuit.calculate());
+        input1.set(false);
+        input2.set(false);
+        assertEquals(false, circuit.calculate());
 
-         System.out.println("Success!");
-     }
+        input1.set(true);
+        input2.set(false);
+        assertEquals(false, circuit.calculate());
 
-     @Test
-     public void testOr() {
-         final InputBoolean input1 = factory.createInputBoolean(false);
-         final InputBoolean input2 = factory.createInputBoolean(true);
-         Circuit circuit = factory.createCircuit(factory.createOrGateWithInputs(input1, input2));
-         assertEquals(true, circuit.calculate());
+        System.out.println("Success!");
+    }
 
-         input1.set(true);
-         input2.set(true);
-         assertEquals(true, circuit.calculate());
+    @Test
+    public void testOrGate() {
+        final InputBoolean input1 = factory.createInputBoolean(false);
+        final InputBoolean input2 = factory.createInputBoolean(true);
+        Circuit circuit = factory.createCircuit(factory.createOrGateWithInputs(input1, input2));
+        assertEquals(true, circuit.calculate());
 
-         input1.set(false);
-         input2.set(false);
-         assertEquals(false, circuit.calculate());
+        input1.set(true);
+        input2.set(true);
+        assertEquals(true, circuit.calculate());
 
-         input1.set(true);
-         input2.set(false);
-         assertEquals(true, circuit.calculate());
+        input1.set(false);
+        input2.set(false);
+        assertEquals(false, circuit.calculate());
 
-         System.out.println("Test OR success!");
-     }
-     
-     
-     @Test
-     public void test2() {
-         final InputBoolean input1 = factory.createInputBoolean(false);
-         final InputBoolean input2 = factory.createInputBoolean(true);
-         Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
-         assertEquals(false, circuit.calculate());
-         
-         input1.set(true);
-         assertEquals(true, circuit.calculate());
-         System.out.println("Success!");
-     }
-     
-     @Test
-     public void test3() {
-         final InputBoolean input1 = factory.createInputBoolean(true);
-         final InputBoolean input2 = factory.createInputBoolean(false);
-         final InputBoolean input3 = factory.createInputBoolean(false);
-         Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, factory.createOrGateWithInputs(input2, input3)));
-         assertEquals(false, circuit.calculate());
-         
-         input3.set(true);
-         assertEquals(true, circuit.calculate());
-         System.out.println("Success!");
-     }
-     
-     @Test
-     public void testNot() {
-         final InputBoolean input1 = factory.createInputBoolean(true);
-         Circuit circuit = factory.createCircuit(factory.createNotGateWithInput(input1));
-         assertEquals(false, circuit.calculate());
-         
-         input1.set(false);
-         assertEquals(true, circuit.calculate());
-         System.out.println("Success!");
-     }
-     
-     
-     @Test
-     public void hello() {
-         Circuit circuit = factory.createCircuit(factory.createInputBoolean(false));
-         circuit.hello();
-         System.out.println("Success!");
-     }
-     
-     
-    
+        input1.set(true);
+        input2.set(false);
+        assertEquals(true, circuit.calculate());
+
+        System.out.println("Test OR success!");
+    }
+
+
+    @Test
+    public void testIfCircuitIsFailSafe1() {
+        try {
+            final InputBoolean input1 = null;
+            final InputBoolean input2 = factory.createInputBoolean(true);
+
+            Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
+            circuit.calculate();
+            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
+        } catch (final Exception e) {
+            System.out.println("Exception was thrown. This was the desired behaviour");
+        }
+    }
+
+    @Test
+    public void testIfCircuitIsFailSafe2() {
+        try {
+            final InputBoolean input1 = factory.createInputBoolean(false);
+            final InputBoolean input2 = null;
+
+            Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
+            circuit.calculate();
+            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
+        } catch (final Exception e) {
+            System.out.println("Exception was thrown. This was the desired behaviour");
+        }
+    }
+
+    @Test
+    public void testIfCircuitIsFailSafe3() {
+        try {
+            final InputBoolean input1 = factory.createInputBoolean(false);
+            final InputBoolean input2 = factory.createInputBoolean(true);
+            final Calculatable sig1 = factory.createAndGateWithInputs(input1, input2);
+            final Calculatable sig2 = factory.createNotGateWithInput(null);
+            final Calculatable sig3 = factory.createOrGateWithInputs(sig1, sig2);
+            Circuit circuit = factory.createCircuit(sig1);
+            circuit.calculate();
+            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
+        } catch (final Exception e) {
+            System.out.println("Exception was thrown. This was the desired behaviour");
+        }
+
+        System.out.println("Exceptions handled correctly!");
+    }
+
+    @Test
+    public void test3() {
+        final InputBoolean input1 = factory.createInputBoolean(true);
+        final InputBoolean input2 = factory.createInputBoolean(false);
+        final InputBoolean input3 = factory.createInputBoolean(false);
+        Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, factory.createOrGateWithInputs(input2, input3)));
+        assertEquals(false, circuit.calculate());
+
+        input3.set(true);
+        assertEquals(true, circuit.calculate());
+        System.out.println("Success!");
+    }
+
+    @Test
+    public void testNotGate() {
+        final InputBoolean input1 = factory.createInputBoolean(true);
+        Circuit circuit = factory.createCircuit(factory.createNotGateWithInput(input1));
+        assertEquals(false, circuit.calculate());
+
+        input1.set(false);
+        assertEquals(true, circuit.calculate());
+        System.out.println("Success!");
+    }
+
+
+    @Test
+    public void hello() {
+        Circuit circuit = factory.createCircuit(factory.createInputBoolean(false));
+        circuit.hello();
+        System.out.println("Success!");
+    }
+
+
     public Testing() {
     }
 
