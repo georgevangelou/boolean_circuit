1e9ea75bd8d60067ee10051c44d26e0c5a56c282
Georgios Evangelou <george_evangelou@windowslive.com>
2022-01-06 17:20:45 +0200

    Moved tests around

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -11,6 +11,13 @@ public class Testing {
     private ElementFactory factory = new ElementFactory(API.VERSION);
 
     @Test
+    public void testCreation() {
+        Circuit circuit = factory.createCircuit(factory.createInputBoolean(false));
+        circuit.hello();
+        System.out.println("Success!");
+    }
+
+    @Test
     public void testAndGate() {
         final InputBoolean input1 = factory.createInputBoolean(false);
         final InputBoolean input2 = factory.createInputBoolean(true);
@@ -52,6 +59,32 @@ public class Testing {
         assertEquals(true, circuit.calculate());
 
         System.out.println("Test OR success!");
+    }
+
+
+    @Test
+    public void testNotGate() {
+        final InputBoolean input1 = factory.createInputBoolean(true);
+        Circuit circuit = factory.createCircuit(factory.createNotGateWithInput(input1));
+        assertEquals(false, circuit.calculate());
+
+        input1.set(false);
+        assertEquals(true, circuit.calculate());
+        System.out.println("Success!");
+    }
+    
+
+    @Test
+    public void test3() {
+        final InputBoolean input1 = factory.createInputBoolean(true);
+        final InputBoolean input2 = factory.createInputBoolean(false);
+        final InputBoolean input3 = factory.createInputBoolean(false);
+        Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, factory.createOrGateWithInputs(input2, input3)));
+        assertEquals(false, circuit.calculate());
+
+        input3.set(true);
+        assertEquals(true, circuit.calculate());
+        System.out.println("Success!");
     }
 
 
@@ -101,38 +134,6 @@ public class Testing {
         System.out.println("Exceptions handled correctly!");
     }
 
-    @Test
-    public void test3() {
-        final InputBoolean input1 = factory.createInputBoolean(true);
-        final InputBoolean input2 = factory.createInputBoolean(false);
-        final InputBoolean input3 = factory.createInputBoolean(false);
-        Circuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, factory.createOrGateWithInputs(input2, input3)));
-        assertEquals(false, circuit.calculate());
-
-        input3.set(true);
-        assertEquals(true, circuit.calculate());
-        System.out.println("Success!");
-    }
-
-    @Test
-    public void testNotGate() {
-        final InputBoolean input1 = factory.createInputBoolean(true);
-        Circuit circuit = factory.createCircuit(factory.createNotGateWithInput(input1));
-        assertEquals(false, circuit.calculate());
-
-        input1.set(false);
-        assertEquals(true, circuit.calculate());
-        System.out.println("Success!");
-    }
-
-
-    @Test
-    public void hello() {
-        Circuit circuit = factory.createCircuit(factory.createInputBoolean(false));
-        circuit.hello();
-        System.out.println("Success!");
-    }
-
 
     public Testing() {
     }
@@ -152,22 +153,4 @@ public class Testing {
     @org.junit.jupiter.api.AfterEach
     public void tearDown() throws Exception {
     }
-//    
-//    @BeforeAll
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterAll
-//    public static void tearDownClass() {
-//    }
-//    
-//    @BeforeEach
-//    public void setUp() {
-//    }
-//    
-//    @AfterEach
-//    public void tearDown() {
-//    }
-
-
 }
