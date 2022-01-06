d3ecfda6f2b25ede57f509b9fc58fdd5fa5f764e
George Evangelou <george_evangelou@windowslive.com>
2022-01-06 12:29:15 +0200

    Implemented And gate

--- src/main/java/circuitry/gates/And.java
+++ src/main/java/circuitry/gates/And.java
@@ -3,6 +3,8 @@ package circuitry.gates;
 /**
  *
  */
-public class And {
-    
+public class And extends Gate {
+    protected boolean calculate(final boolean a, final boolean b) {
+        return a && b;
+    }
 }
