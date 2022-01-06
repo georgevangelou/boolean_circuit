37b9257bc42af9cd71e0adfcf65b7db742ac61fb
Mayank Sharma <mayankshar21@yahoo.com.sg>
2022-01-06 11:55:18 +0100

    Added OR gate and gate factory

--- src/main/java/circuitry/gates/Or.java
+++ src/main/java/circuitry/gates/Or.java
@@ -3,6 +3,9 @@ package circuitry.gates;
 /**
  *
  */
-public class Or {
-    
+public class Or extends Gate{
+    @Override
+    protected boolean calculate(final boolean a, final boolean b) {
+        return a || b;
+    }
 }
