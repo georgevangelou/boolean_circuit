9c6aa099919ba1cd519edc48473d60bcda5df1cd
Georgios Evangelou <george_evangelou@windowslive.com>
2022-01-07 16:43:46 +0200

    Removed deprecated code + added wildcards

--- src/test/java/TestingDualModeCircuit.java
+++ src/test/java/TestingDualModeCircuit.java
@@ -1,7 +1,3 @@
-import circuitry.elements.API;
-import circuitry.elements.DualModeCircuit;
-import circuitry.elements.ElementFactory;
-import circuitry.elements.InputPair;
 import circuitry.elements.*;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
@@ -171,23 +167,23 @@ public class TestingDualModeCircuit {
         final InputPair<?> input3 = factory.createInputPair(true, true);
         final InputPair<?> input4 = factory.createInputPair(true, true);
 
-        final Calculatable sig1 = factory.createDualModeNotGateWithInputs(input1);
-        final Calculatable sig2 = factory.createDualModeAndGateWithInputs(sig1, input2);
-        final Calculatable sig3 = factory.createDualModeAndGateWithInputs(input3, input4);
-        final Calculatable sig4 = factory.createDualModeOrGateWithInputs(sig3, sig2);
+        final Calculatable<?> sig1 = factory.createDualModeNotGateWithInputs(input1);
+        final Calculatable<?> sig2 = factory.createDualModeAndGateWithInputs(sig1, input2);
+        final Calculatable<?> sig3 = factory.createDualModeAndGateWithInputs(input3, input4);
+        final Calculatable<?> sig4 = factory.createDualModeOrGateWithInputs(sig3, sig2);
 
         Circuit circuit = factory.createDualModeCircuit(sig4);
         result = circuit.calculatePairOutput();
-        assertEquals( Pair.of(true,true), result);
+        assertEquals(Pair.of(true, true), result);
 
         input1.set(factory.createInputPair(true, false));
         input2.set(factory.createInputPair(true, false));
         input3.set(factory.createInputPair(true, false));
 
-        final Calculatable sig5 = factory.createDualModeNotGateWithInputs(input1);
-        final Calculatable sig6 = factory.createDualModeAndGateWithInputs(sig5, input2);
-        final Calculatable sig7 = factory.createDualModeAndGateWithInputs(input3, input4);
-        final Calculatable sig8 = factory.createDualModeOrGateWithInputs(sig7, sig6);
+        final Calculatable<?> sig5 = factory.createDualModeNotGateWithInputs(input1);
+        final Calculatable<?> sig6 = factory.createDualModeAndGateWithInputs(sig5, input2);
+        final Calculatable<?> sig7 = factory.createDualModeAndGateWithInputs(input3, input4);
+        final Calculatable<?> sig8 = factory.createDualModeOrGateWithInputs(sig7, sig6);
 
         Circuit circuit1 = factory.createDualModeCircuit(sig8);
         result = circuit1.calculatePairOutput();
@@ -202,8 +198,8 @@ public class TestingDualModeCircuit {
         } catch (Exception e) {
         }
         System.out.println("Success (testComplexCircuitDouble)!");
-
     }
+
 
     @Test
     public void testGteGateBoolean() {
@@ -250,77 +246,6 @@ public class TestingDualModeCircuit {
         }
         System.out.println("Success (testGteGateDouble)!");
     }
-
-//
-//    @Test
-//    public void testComplexCircuit() {
-//        final InputPair a = factory.createInputPair(true, true);
-//        final InputPair b = factory.createInputPair(false, 0);
-//        final InputPair c = factory.createInputPair(false, 0);
-//
-//        final Calculatable sig1 = factory.createOrGateWithInputs(a, b);
-//        final Calculatable sig2 = factory.createNotGateWithInput(c);
-//        final Calculatable sig3 = factory.createAndGateWithInputs(sig1, sig2);
-//
-//        // circuit = (a or b) and (!c)
-//        BooleanDoubleCircuit circuit = factory.createCircuit(sig3);
-//
-//        assertEquals(true, circuit.calculate());
-//
-//        c.set(true);
-//        assertEquals(false, circuit.calculate());
-//        System.out.println("Success (testComplexCircuit)!");
-//    }
-//
-//
-//    @Test
-//    public void testIfCircuitIsFailSafe1() {
-//        try {
-//            final InputPair input1 = null;
-//            final InputPair input2 = factory.createInputPair(true, true);
-//
-//            BooleanDoubleCircuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
-//            circuit.calculate();
-//            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
-//        } catch (final Exception e) {
-////            System.out.println("Exception was thrown. This was the desired behaviour");
-//            System.out.println("Success (testIfCircuitIsFailSafe1)!");
-//        }
-//    }
-//
-//
-//    @Test
-//    public void testIfCircuitIsFailSafe2() {
-//        try {
-//            final InputPair input1 = factory.createInputPair(false, 0);
-//            final InputPair input2 = null;
-//
-//            BooleanDoubleCircuit circuit = factory.createCircuit(factory.createAndGateWithInputs(input1, input2));
-//            circuit.calculate();
-//            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
-//        } catch (final Exception e) {
-////            System.out.println("Exception was thrown. This was the desired behaviour");
-//            System.out.println("Success (testIfCircuitIsFailSafe2)!");
-//        }
-//    }
-//
-//
-//    @Test
-//    public void testIfCircuitIsFailSafe3() {
-//        try {
-//            final InputPair input1 = factory.createInputPair(false, 0);
-//            final InputPair input2 = factory.createInputPair(true, true);
-//            final Calculatable sig1 = factory.createAndGateWithInputs(input1, input2);
-//            final Calculatable sig2 = factory.createNotGateWithInput(null);
-//            final Calculatable sig3 = factory.createOrGateWithInputs(sig1, sig2);
-//            BooleanDoubleCircuit circuit = factory.createCircuit(sig3);
-//            circuit.calculate();
-//            fail("Wrong user-input or circuit connections were handled INCORRECTLY");
-//        } catch (final Exception e) {
-////            System.out.println("Exception was thrown. This was the desired behaviour");
-//            System.out.println("Success (testIfCircuitIsFailSafe3)!");
-//        }
-//    }
 
 
     public TestingDualModeCircuit() {
