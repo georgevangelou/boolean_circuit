281c4685d1a56d34a657656371773c3347209e42
Georgios Evangelou <george_evangelou@windowslive.com>
2022-01-07 13:57:48 +0200

    Implemented Gte

---
+++ src/main/java/circuitry/elements/Gte.java
@@ -0,0 +1,34 @@
+package circuitry.elements;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ *
+ */
+public class Gte<T> extends Calculatable<T> {
+    private final Calculatable<Double> a;
+    private final Calculatable<Double> b;
+    private final Class<Double> classOfInputs = Double.class;
+
+
+    protected Gte(final Calculatable<Double> a, final Calculatable<Double> b) {
+        Preconditions.checkNotNull(a, "Gate input should not be null");
+        Preconditions.checkNotNull(b, "Gate input should not be null");
+        Preconditions.checkNotNull(classOfInputs, "Gate input should not be null");
+
+        this.a = a;
+        this.b = b;
+    }
+
+
+    @Override
+    protected Pair<Boolean, T> calculateInner() {
+        if (this.classOfInputs == Double.class) {
+            Boolean value =  ((Double) a.calculate().getRight() >= ((Double) b.calculate().getRight()));
+            return Pair.of(false, (T) value);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}
