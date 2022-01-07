cef7c29780d50d0c6fc1ee88816676c1bf14ed26
Georgios Evangelou <george_evangelou@windowslive.com>
2022-01-07 15:35:56 +0200

    Moved methods from new factory to old factory (pt.1)

--- src/main/java/circuitry/elements/ElementFactory.java
+++ src/main/java/circuitry/elements/ElementFactory.java
@@ -1,6 +1,9 @@
 package circuitry.elements;
 
 import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.InputMismatchException;
 
 /**
  *
@@ -50,5 +53,73 @@ public final class ElementFactory extends API {
         Preconditions.checkNotNull(a, "Gate input should not be null");
 
         return new Not<Boolean>(a, Boolean.class);
+    }
+
+
+
+    public final DualModeCircuit createDualModeCircuit(final Calculatable<?> a) {
+        Preconditions.checkNotNull(a, "Circuit input should not be null");
+
+        return new DualModeCircuit((Calculatable<?>) a);
+    }
+
+
+    public final InputPair<?> createInputPair(final boolean valueIsBoolean, final Object value) {
+        if (((value.getClass() == Double.class) && !valueIsBoolean) || ((value.getClass() == Boolean.class) && valueIsBoolean)) {
+            return new InputPair<>(Pair.of(valueIsBoolean, value));
+        }
+        throw new InputMismatchException("Wrong input pair");
+    }
+
+
+    public final Calculatable<?> createDualModeAndGateWithInputs(final Calculatable<?> a, final Calculatable<?> b) {
+        Preconditions.checkNotNull(a, "Gate input should not be null");
+        Preconditions.checkNotNull(b, "Gate input should not be null");
+
+        if (a.calculate().getRight() instanceof Boolean) {
+            return new And<Boolean>((Calculatable<Boolean>) a, (Calculatable<Boolean>) b, Boolean.class);
+        } else if (a.calculate().getRight() instanceof Double) {
+            return new And<Double>((Calculatable<Double>) a, (Calculatable<Double>) b, Double.class);
+        } else {
+            throw new InputMismatchException("Wrong matching");
+        }
+    }
+
+    public final Calculatable<?> createDualModeOrGateWithInputs(final Calculatable<?> a, final Calculatable<?> b) {
+        Preconditions.checkNotNull(a, "Gate input should not be null");
+        Preconditions.checkNotNull(b, "Gate input should not be null");
+
+        if (a.calculate().getRight() instanceof Boolean) {
+            return new Or<Boolean>((Calculatable<Boolean>) a, (Calculatable<Boolean>) b, Boolean.class);
+        } else if (a.calculate().getRight() instanceof Double) {
+            return new Or<Double>((Calculatable<Double>) a, (Calculatable<Double>) b, Double.class);
+        } else {
+            throw new InputMismatchException("Wrong matching");
+        }
+    }
+
+
+    public final Calculatable<?> createDualModeNotGateWithInputs(final Calculatable<?> a) {
+        Preconditions.checkNotNull(a, "Gate input should not be null");
+
+        if (a.calculate().getRight() instanceof Boolean) {
+            return new Not<Boolean>((Calculatable<Boolean>) a, Boolean.class);
+        } else if (a.calculate().getRight() instanceof Double) {
+            return new Not<Double>((Calculatable<Double>) a, Double.class);
+        } else {
+            throw new InputMismatchException("Wrong matching");
+        }
+    }
+
+
+    public final Calculatable<Boolean> createGteGateWithInputs(final Calculatable<?> a, final Calculatable<?> b) {
+        Preconditions.checkNotNull(a, "Gate input should not be null");
+        Preconditions.checkNotNull(b, "Gate input should not be null");
+
+        if (a.calculate().getRight() instanceof Double && b.calculate().getRight() instanceof Double) {
+            return new Gte<>((Calculatable<Double>) a, (Calculatable<Double>) b);
+        } else {
+            throw new InputMismatchException("Wrong matching");
+        }
     }
 }
